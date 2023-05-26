15cb304b3b4d328ca1050455881b92aafd02e16c
Jakub Zięba <jakubzieba7@gmail.com>
2023-05-26 13:56:58 +0200

    Sending errors

---
+++ Repositories/ErrorRepository.cs
@@ -0,0 +1,21 @@
+﻿using SMSReportService.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSReportService.Repositories
+{
+    public class ErrorRepository
+    {
+        public List<Error> GetLastErrors(int intervalInMinutes)
+        {
+            return new List<Error>
+            {
+                new Error{Message="Błąd testowy 1",Date=DateTime.Now },
+                new Error{Message="Błąd testowy 2",Date=DateTime.Now }
+            };
+        }
+    }
+}
--- SMSReportService.cs
+++ SMSReportService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMSReportService.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,16 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace SMSReportService
 {
     public partial class SMSReportService : ServiceBase
     {
+        private const int IntervalInMinutes = 30;
+        private Timer _timer=new Timer(IntervalInMinutes*1000);
+        private ErrorRepository _errorRepository = new ErrorRepository();
+
         public SMSReportService()
         {
             InitializeComponent();
@@ -19,7 +25,23 @@ namespace SMSReportService
 
         protected override void OnStart(string[] args)
         {
+            _timer.Elapsed += DoWork;
+            _timer.Start();
         }
+
+        private void SendError()
+        {
+            var errors = _errorRepository.GetLastErrors(IntervalInMinutes);
+
+            if (errors == null || !errors.Any())
+                return;
+        }
+
+        private void DoWork(object sender, ElapsedEventArgs e)
+        {
+            SendError();
+        }
+
 
         protected override void OnStop()
         {
